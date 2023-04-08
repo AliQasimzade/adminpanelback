105c3e364a9caec6c703d1b2fbf457b8f4660b03
Ali Qasimzade <ali@globalsoft.az>
2023-04-09 00:51:22 +0400

    fixed

--- Model/ListingsModel.js
+++ Model/ListingsModel.js
@@ -2,33 +2,51 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listings = new Schema({
-    post_author: { type: String },
-    post_date: { type: Date, default: Date.now },
-    post_date_gmt: { type: Date, default: Date.now },
-    post_content: { type: String },
-    post_title: { type: String },
-    post_excerpt: { type: String },
-    post_status: { type: String },
-    comment_status: { type: String },
-    ping_status: { type: String },
-    post_password: { type: String },
-    post_name: { type: String },
-    to_ping: { type: String },
-    pinged: { type: String },
-    post_modified: { type: Date, default: Date.now },
-    post_modified_gmt: { type: Date, default: Date.now },
-    post_content_filtered: { type: String },
-    post_parent: { type: Number },
-    guid: { type: String },
-    menu_order: { type: Number },
-    post_type: { type: String },
-    post_mime_type: { type: String },
-    comment_count: { type: Number },
-    filter: { type: String },
-    address: { type: String },
-    phone: { type: String },
-    status: { type: String },
-    image: {
+    email: {
+        type: String,
+        require: true
+    },
+    twitter: {
+        type: String,
+        require: true
+    },
+    uploadlink: {
+        type: String,
+        require: true
+    },
+    dribble: {
+        type: String,
+        require: true
+    },
+    address: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true
+    },
+    zipcode: {
+        type: Number,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    linkedin: {
+        type: String,
+        require: true
+    },
+    facebook: {
+        type: String,
+        require: true
+    },
+    whatsapp: {
+        type: String,
+        require: true
+    },
+    website: {
         type: String,
         require: true
     },
@@ -36,26 +54,56 @@ const listings = new Schema({
         type: String,
         require: true
     },
-    links:{
-     self: {
-        href: {
-            type:String
-        }
-     },
-     collection:{
-        href:{
-            type:String
-        }
-     }
+    listingTitle: {
+        type: String,
+        require: true
     },
-    rating_avg: { type: Number, default: 0 },
-    rating_count: { type: Number, default: 0 },
-    wishlist: { type: Boolean, default: false },
-    booking_use: { type: Boolean, default: false },
-    booking_style: { type: String },
-    booking_price: { type: String },
-    booking_price_display: { type: String }
-});
+    image: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true
+    },
+    previousprice: {
+        type: Number,
+        require: true
+    },
+
+    roadorstate: {
+        type: String,
+        require: true
+    },
+    cityorstate: {
+        type: String,
+        require: true
+    },
+    splashscreen: {
+        type: String,
+        require: true
+    },
+    profileImage:{
+        type:String,
+        require:true
+    },
+    features: {
+        type: Object,
+        require: true
+    },
+    slogan: {
+        type: String,
+        require: true
+    },
+    tags: {
+        type: Object,
+        require: true
+    },
+    timeschedule: {
+        type: Object,
+        require: true
+    }
+})
 
 
 const Listings = mongoose.model("listings", listings);
